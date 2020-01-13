08ce3594ddf56dc99865c6eb0f4f5cc641898ad4
Annaia Berry <jarcane@gmail.com>
2020-01-13 14:37:15 +0200

    Keyboard input

--- src/arcadia/core.cljs
+++ src/arcadia/core.cljs
@@ -3,24 +3,30 @@
 
 (enable-console-print!)
 
-(println "This text is printed from src/arcadia/core.cljs. Go ahead and edit it and see reloading in action.")
-
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom 1))
 
-(defn start []
-  (let [canvas (js/document.getElementById "game")
-        ctx (.getContext canvas "2d")]
-    (.clearRect ctx 0 0 256 256)
-    (.fillText ctx @app-state 64 128)
-    (swap! app-state inc)
-    (js/requestAnimationFrame start)))
+(defn game-loop! [ctx keys]
+  (.clearRect ctx 0 0 256 256)
+  (.fillText ctx @app-state 64 128)
+  (cond
+    (get @keys "ArrowUp") (swap! app-state inc)
+    (get @keys "ArrowDown") (swap! app-state dec))
+  (js/requestAnimationFrame (fn [] (game-loop! ctx keys))))
 
-(start)
+(defn start! []
+  (let [canvas (js/document.getElementById "game")
+        ctx (.getContext canvas "2d")
+        keys (atom {})]
+    (set! js/window.onkeyup (fn [e] (swap! keys assoc (.-code e) false)))
+    (set! js/window.onkeydown (fn [e] (swap! keys assoc (.-code e) true)))
+    (game-loop! ctx keys)))
+
+
+(start!)
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-  )
+  (reset! app-state 0))
