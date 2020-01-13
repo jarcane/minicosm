b2184279d9c701c771234e791f12ffe401e00145
Annaia Berry <jarcane@gmail.com>
2020-01-13 20:08:00 +0200

    Remove scaling until figwheel bug can be resolved

--- src/arcadia/core.cljs
+++ src/arcadia/core.cljs
@@ -18,20 +18,21 @@
         init-state (init)]
     (set! js/window.onkeyup (fn [e] (swap! key-evs assoc (.-code e) false)))
     (set! js/window.onkeydown (fn [e] (swap! key-evs assoc (.-code e) true)))
-    (.scale ctx 2 2)
     (game-loop! 0 ctx key-evs init-state handlers)))
 
-(start!
- {:init (fn [] [128 128])
-  :on-key (fn [[x y] key-evs]
-            (cond
-              (get key-evs "ArrowUp") [x (dec y)]
-              (get key-evs "ArrowDown") [x (inc y)]
-              (get key-evs "ArrowLeft") [(dec x) y]
-              (get key-evs "ArrowRight") [(inc x) y]
-              :else [x y]))
-  :on-tick (fn [state _] state)
-  :to-draw (fn [[x y] ctx]
-             (def img (js/Image.))
-             (set! (.-src img) "https://media.giphy.com/media/NMr9UUZSqQbhS/giphy.gif")
-             (.drawImage ctx img x y))})
+(def game-handlers
+  {:init (fn [] [128 128])
+   :on-key (fn [[x y] key-evs]
+             (cond
+               (get key-evs "ArrowUp") [x (- y 3)]
+               (get key-evs "ArrowDown") [x (+ y 3)]
+               (get key-evs "ArrowLeft") [(- x 3) y]
+               (get key-evs "ArrowRight") [(+ x 3) y]
+               :else [x y]))
+   :on-tick (fn [state _] state)
+   :to-draw (fn [[x y] ctx]
+              (def img (js/Image.))
+              (set! (.-src img) "https://media.giphy.com/media/NMr9UUZSqQbhS/giphy.gif")
+              (.drawImage ctx img x y))})
+
+(start! game-handlers)
