9cf09af2d9c2350b339fbf39995eda906a99bf9b
Annaia Danvers <jarcane@users.noreply.github.com>
2020-03-27 10:24:11 +0200

    Use render-to-canvas to gen demo starfield

--- src/minicosm/demo.cljs
+++ src/minicosm/demo.cljs
@@ -1,50 +1,40 @@
 (ns minicosm.demo
   (:require [minicosm.core :refer [start!]]
-            [minicosm.image :refer [sprite8 sprite16]]))
+            [minicosm.ddn :refer [render-to-canvas]]))
 
 (enable-console-print!)
 
-(def _ 0x00000000)
-(def O 0xFFFFFFFF)
-(def X 0xFF000000)
-
-(def sprite
-  (sprite8 [_ _ _ O O _ _ _
-            _ _ O X X O _ _
-            _ _ O X X O _ _
-            _ O X O O X O _
-            _ O X X X X O _
-            _ O X X X X O _
-            O X X O O X X O
-            O X O _ _ O X O]))
-
 (defn make-stars []
-  (sprite16 
-   (for [_ (range 256)]
-     (if (< (rand) 0.005)
-       O
-       X))))
+  (render-to-canvas 16 16
+   [:canvas {}
+    [:rect {:pos [0 0] :dim [16 16] :color "black" :style :fill}]
+    (concat [:group {}]
+            (for [_ (range 3)]
+              [:point {:pos [(rand-int 16) (rand-int 16)] 
+                       :color (rand-nth ["white" "white" "white" "yellow" "blue" "red"])}]))]))
 
-(def tilemap 
+(def tilemap
   (for [y (range 24)]
     (for [x (range 32)]
       (make-stars))))
 
 (defn draw [[x y] assets]
   [:canvas {}
-   [:image {:pos [0 0]} (:space assets)]
-   #_ [:map {:pos [0 0] :dim [32 24] :size 16} tilemap]
-   [:sprite {:pos [x y]} sprite]
+   #_ [:image {:pos [0 0]} (:space assets)]
+   [:map {:pos [0 0] :dim [32 24] :size 16} tilemap]
+   [:sprite {:pos [x y]} (:ship assets)]
    [:text {:pos [32 32] :color "white" :font "16px serif"} "THIS IS A TEST"]
    [:group {:desc "lines"}
     [:rect {:style :fill :pos [300 200] :dim [64 32] :color "white"}]
     [:rect {:pos [200 150] :dim [32 32] :color "white"}]
     [:circ {:pos [400 50] :r [32 32] :color "white"}]
-    [:line {:from [200 64] :to [350 150] :color "white"}]]])
+    [:line {:from [200 64] :to [350 150] :color "white"}]
+    [:point {:pos [45 100] :color "purple"}]]])
 
 (def game-handlers
   {:init (fn [] [128 128])
-   :assets (fn [] {:space "https://images.pexels.com/photos/957010/milky-way-starry-sky-night-sky-star-957010.jpeg"})
+   :assets (fn [] {:space "/img/starfield.gif"
+                   :ship "/img/shuttle.png"})
    :on-key (fn [[x y] key-evs]
              (cond
                (key-evs "ArrowUp") [x (- y 3)]
