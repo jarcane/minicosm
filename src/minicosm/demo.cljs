0921f2d061bfc8b98f6f72d523f6c280d2aed978
C. Warren Dale <adventureonthehighseas@gmail.com>
2020-06-28 11:21:37 -0500

    Hover sound

--- src/minicosm/demo.cljs
+++ src/minicosm/demo.cljs
@@ -38,22 +38,30 @@
    [:sprite {:pos [(:x state) (:y state)]} (:ship assets)]])
 
 (def game-handlers
-  {:init (fn [] {:x 128 :y 128 :zap false})
+  {:init (fn [] {:x 128 :y 128 :moving false})
    :assets (fn [] {:ship [:image "/img/shuttle.png"]
                    :zap [:audio "/audio/zap.wav"]
+                   :hover [:audio "/audio/hover.wav"]
                    :background [:audio "/audio/igluifohn.wav"]})
    :on-key (fn [state key-evs]
              (cond-> state
+               true (assoc :moving
+                           (pos? (count
+                               (clojure.set/intersection
+                                 #{"ArrowUp" "ArrowDown" "ArrowLeft" "ArrowRight"}
+                                 key-evs))))
                (key-evs "ArrowUp") (update :y dec)
                (key-evs "ArrowDown") (update :y inc)
                (key-evs "ArrowLeft") (update :x dec)
                (key-evs "ArrowRight") (update :x inc)
-               (key-evs "Space") (update :zap not)
                :else identity))
    :on-tick (fn [state _] state)
    :to-play (fn [state assets sounds]
-              {:music (if (> (:y state) 100) #{} #{(:background assets)})
-               :effects (if (:zap state) #{(:zap assets)} #{})})
+              {:music #{(:background assets)}
+               :effects (if
+                          (:moving state)
+                          #{(:hover assets)} 
+                          #{})})
    :to-draw draw})
 
 (start! game-handlers)
